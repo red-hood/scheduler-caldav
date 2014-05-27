f77721bbb328fd291a4e210dd6450fde45f3e3c4
Christian Roeder <christian.roeder@red-hood.de>
2014-05-28 01:17:42 +0200

    restructure

--- scheduler.py
+++ scheduler.py
@@ -4,28 +4,8 @@ import sys
 time_format = '%Y-%m-%d %H:%M'
 
 
-# TODO: expand RRULES
-# create XML from dst/xslt
-# make class from scheduler event with class methods
-def schedulerEvent(id, start, end, text=""):
-        root = Element('event', id=id)
-
-        # SubElement does not allow to specify a CDATA element in the text
-        # attribute during creation
-        if text is None:
-            text = ""
-        ctext = CDATA(text)
-        elem_text = Element('text')
-        elem_text.text = ctext
-        root.append(elem_text)
-
-        start_elem = SubElement(root, 'start_date')
-        start_elem.text = start.strftime(time_format)
-        end_elem = SubElement(root, 'end_date')
-        end_elem.text = end.strftime(time_format)
-        return root
-
-
+# helper  method
+# TODO add to event wrapper class
 def _getEventValue(calevent, key):
     try:
         val = getattr(calevent.instance.vevent, key).value
@@ -42,40 +22,76 @@ def _getEventValue(calevent, key):
     return val
 
 
-def _schedulerEventfromCalEv(calevent):
-    ical_scheduler_map = {
-        'id': 'uid',
-        'text': 'summary',
-        'start': 'dtstart',
-        'end': 'dtend',
-    }
+class SchedulerEvent(object):
+    def __init__(self, id, start, end, text=""):
+        if text is None:
+            text = ""
 
-    scheduler_vals = {key: _getEventValue(calevent, val) for key, val
-                      in ical_scheduler_map.iteritems()}
+        self.id = id
+        self.start = start
+        self.end = end
+        self.text = text
 
-    return schedulerEvent(**scheduler_vals)
+    def toXml(self, pretty_print=True):
+        # TODO: output with objectify
+        root = Element('event', id=self.id)
+
+        # SubElement does not allow to specify a CDATA element in the text
+        # attribute during creation
+        ctext = CDATA(self.text)
+        elem_text = Element('text')
+        elem_text.text = ctext
+        root.append(elem_text)
+
+        start_elem = SubElement(root, 'start_date')
+        start_elem.text = self.start.strftime(time_format)
+        end_elem = SubElement(root, 'end_date')
+        end_elem.text = self.end.strftime(time_format)
+        return root
+
+    @classmethod
+    def fromCalEvent(cls, cal_event):
+        ical_scheduler_map = {
+            'id': 'uid',
+            'text': 'summary',
+            'start': 'dtstart',
+            'end': 'dtend',
+        }
+
+        scheduler_vals = {key: _getEventValue(cal_event, val) for key, val
+                          in ical_scheduler_map.iteritems()}
+        return cls(**scheduler_vals)
 
 
-def schedulerEventsfromCaleEvs(calevents):
-    for ev in calevents:
-        ev.load()
+# container for the ScheduverEvents
+class SchedulerCalendar(list):
+    def __init__(self, scheduler_events):
+        super(SchedulerCalendar, self).__init__(scheduler_events)
 
-    # filter for other event types, e.g. VTODO, we can not display them in the
-    # calendar
-    return [_schedulerEventfromCalEv(ev) for ev in calevents if
-            hasattr(ev.instance, 'vevent')]
+    @classmethod
+    def fromCalEvents(cls, cal_events, load=True):
+        if load is True:
+            cls._loadEvents(cal_events)
+        events = [SchedulerEvent.fromCalEvent(ev) for ev in cal_events if
+                  hasattr(ev.instance, 'vevent')]
+        return cls(events)
 
+    @classmethod
+    def fromCalendar(self, caldav_calendar):
+        cal_events = caldav_calendar.events()
+        return self.fromCalEvents(cal_events)
 
-def schedulerContainerData(scheduler_events, pretty_print=True):
-    root = Element('data')
-    for ev in scheduler_events:
-        root.append(ev)
+    # TODO: in container for caldav calendar
+    @staticmethod
+    def _loadEvents(events):
+        for ev in events:
+            ev.load()
 
-    return tostring(root, pretty_print=pretty_print)
+    def toXML(self):
+        root = Element('data')
+        for ev in self:
+            root.append(ev.toXml())
+        return root
 
-
-# just for test, not belongs here
-def schedulerDataFromCal(cal):
-    cevs = cal.events()
-    sevs = schedulerEventsfromCaleEvs(cevs)
-    return schedulerContainerData(sevs)
+    def toXMLString(self):
+        return tostring(self.toXML(), pretty_print=True)
