f69bca2a19aaa69e87055a4a507522640fc1b093
Christian Roeder <christian.roeder@red-hood.de>
2014-10-19 22:51:31 +0200

    implement authenication

---
+++ webapp/auth_mock.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python2
+
+from flask import Flask
+from flask import render_template, Response, request, url_for, abort
+import json
+
+app = Flask(__name__)
+app.debug = True
+
+with open('./acl.json') as acl_file:
+    acl = json.load(acl_file)
+
+
+@app.route('/user/<user>/permissions/calendar')
+def user_permission(user):
+    try:
+        return json.dumps(acl['users'][user]['calendar'])
+    except KeyError:
+        abort(404)
+
+
+@app.route("/user/<user>/permissions/calendar/<cal>")
+def cal_permissions(user, cal):
+    try:
+        return acl['users'][user]['calendar'][cal]
+    except KeyError:
+        abort(404)
+
+
+@app.route('/user/auth')
+def auth():
+    return request.cookies['sid']
+
+if __name__ == '__main__':
+    app.run(port=5001)
